1810792450468f353003e9e735562744b85873fb
esraelif <elifesratunca@gmail.com>
2024-02-26 13:26:07 +0100

    done

--- src/components/main/Card.js
+++ src/components/main/Card.js
@@ -5,19 +5,26 @@ const Card = ({ title, desc, image, date }) => {
 
     return (
 
-        <div className='cards'>
-            <div className='title'>
-                <h1>{title}</h1>
-            </div>
-            <div className='date'>
-                <h1>{date}</h1>
-            </div>
-            <img src={image} alt="" />
-            <div className='desc'>
-                <p>{desc}</p>
-            </div>
-        </div>
+        <>
+
+            {/* 
+            {date.includes("February") && (<div className='cards'>
+                <div className='title'>
+                    <h1>{title}</h1>
+                </div>
+                <div className='date'>
+                    <h1>{date}</h1>
+                </div>
+                <img src={image} alt="" />
+                <div className='desc'>
+                    <p>{desc}</p>
+                </div>
+            </div>)} */}  İçinde sadece February olanları goster dersem,bu kod ile yazılır.
+
+        </>
+
+
     );
-}
+};
 
 export default Card;
