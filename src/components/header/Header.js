950a9e63ee50de24438856a5c3c82e90203e00c5
esraelif <elifesratunca@gmail.com>
2024-02-26 12:34:25 +0100

    mainScssDone

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import "../header/Header.scss"
+
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <span className='title'>Who Are You?</span>
+
+
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/components/main/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Card = ({ title, desc, image, date }) => {
+
+
+    return (
+
+        <div className='cards'>
+            <div className='title'>
+                <h1>{title}</h1>
+            </div>
+            <div className='date'>
+                <h1>{date}</h1>
+            </div>
+            <img src={image} alt="" />
+            <div className='desc'>
+                <p>{desc}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
